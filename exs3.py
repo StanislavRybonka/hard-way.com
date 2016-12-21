df596cbad17ef75e21a6e893cffb4eab614b489e
StanislavRybonka <stanislav1991r@gmail.com>
2016-12-21 23:24:35 +0200

    first initial commit

---
+++ exs3.py
@@ -0,0 +1,33 @@
+#Numbers and some Math
+print "I will now count my chickens:"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true, that 3 + 2 < 5 - 7 ?"
+print 3 + 2  < 5 - 7
+
+print "What is it 3 + 2 ?", 3 + 2
+print "What is it 5 - 7 ?", 5 - 7
+
+print "Oh that's why is False"
+
+print "How about some more?"
+
+print "Is it greater?", 5 > -2
+
+print "Is it greater or equal?", 5 >= -2
+
+print "Is it less or equal?", 5 <= -2
+
+print 5 <= -2
+
+print 2.5 < 3
+
+print 7 % 4
+
+print 7 / 4
